6d7aaecef0cd605762c5e381e27223c6766e27a7
zphoenixz <ing.ramiro.valdez.a@gmail.com>
2020-03-01 17:46:29 -0400

    post persons and relations

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,7 @@ const Person = require('../models/Person');
 const Customer = require('../models/Customer');
 const Employee = require('../models/Employee');
 
+//GETS -----------------------------------------------------------------
 exports.getPersons = (req, res, next) => {
     Person.findAll()
         .then(persons => {
@@ -20,7 +21,6 @@ exports.getPersons = (req, res, next) => {
 exports.getCustomers = (req, res, next) => {
     Customer.findAll()
         .then(customers => {
-
             console.log(customers);
         })
         .catch(err => {
@@ -31,10 +31,112 @@ exports.getCustomers = (req, res, next) => {
 exports.getEmployees = (req, res, next) => {
     Employee.findAll()
         .then(employees => {
-
             console.log(employees);
         })
         .catch(err => {
             console.log(err);
         });
+};
+
+//POSTS -----------------------------------------------------------------
+exports.postEmployee = (req, res, next) => {
+    let firstname = "Ramo"; //req.body.firstname
+    let lastname = "Vald"; //req.body.lastname
+    let phone = "67890195"; //req.body.phone
+    let email = "ramo@gmail.com"; //req.body.email
+    let birthdate = '1992-02-02'; //req.body.birthdate
+    let address = 'calle 2 los penocos'; //req.body.address
+    let idnumber = "12345678"; //req.body.idnumber
+    let nit = "Ramo"; //req.body.nit
+    let lat = 54.1347287; //req.body.lat
+    let longitude = 54.1347287; //req.body.longitude
+
+    let user = "ramirojrr"; //req.body.user
+    let password = "12"; //req.body.password
+
+    Person
+        .create({
+            first_name: firstname,
+            last_name: lastname,
+            phone: phone,
+            email: email,
+            address: address,
+            birth_date: birthdate,
+            idnumber: idnumber,
+            idnumber: idnumber,
+            nit: nit,
+            email: email,
+            lat: lat,
+            longitude: longitude,
+        })
+        .then(person => {
+            // fetchedCart = cart;
+            // return cart.getProducts({
+            //     where: {
+            //         id: prodId
+            //     }
+            // });
+            console.log(person);
+            return Employee
+                .create({
+                    Person_id_p: person.id_p,
+                    user: user,
+                    password: password
+                });
+        })
+        .then((employee) => {
+            console.log(employee);
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postCustomer = (req, res, next) => {
+    let firstname = "Ramo"; //req.body.firstname
+    let lastname = "Vald"; //req.body.lastname
+    let phone = "67890195"; //req.body.phone
+    let email = "ramo@gmail.com"; //req.body.email
+    let birthdate = '1992-02-02'; //req.body.birthdate
+    let address = 'calle 2 los penocos'; //req.body.address
+    let idnumber = "12345678"; //req.body.idnumber
+    let nit = "Ramo"; //req.body.nit
+    let lat = 54.1347287; //req.body.lat
+    let longitude = 54.1347287; //req.body.longitude
+
+    let customerStatus = true; //req.body.password
+
+    let dateObj = new Date();
+    let month = dateObj.getUTCMonth() + 1; //months from 1-12
+    let day = dateObj.getUTCDate();
+    let year = dateObj.getUTCFullYear();
+
+    let registerDate = year + "/" + month + "/" + day;
+
+    Person
+        .create({
+            first_name: firstname,
+            last_name: lastname,
+            phone: phone,
+            email: email,
+            address: address,
+            birth_date: birthdate,
+            idnumber: idnumber,
+            idnumber: idnumber,
+            nit: nit,
+            email: email,
+            lat: lat,
+            longitude: longitude,
+        })
+        .then(person => {
+            console.log(person);
+            return Customer
+                .create({
+                    Person_id_p: person.id_p,
+                    customer_status: customerStatus,
+                    register_date: registerDate
+                });
+        })
+        .then((employee) => {
+            console.log(employee);
+        })
+        .catch(err => console.log(err));
 };
--- routes/admin.js
+++ routes/admin.js
@@ -6,9 +6,14 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product => GET
+//GETS -----------------------------------------------------------
 router.get('/persons/all', adminController.getPersons);
 router.get('/customers/all', adminController.getCustomers);
 router.get('/employees/all', adminController.getEmployees);
+
+
+//POSTS -----------------------------------------------------------
+router.get('/employee/add', adminController.postEmployee);
+router.get('/customers/add', adminController.postCustomer);
 
 module.exports = router;
